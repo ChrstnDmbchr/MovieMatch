807710483bdec8c3f2da428e7ae0d3f11f5a999e
chrstndmbchr <christiandambacher@gmail.com>
2021-01-18 20:07:43 +0000

    swipe triggers working

--- src/app/components/swipe-card/swipe-card.component.ts
+++ src/app/components/swipe-card/swipe-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
-
+const SWIPE_THRESHOLD = 100;
 @Component({
   selector: 'app-swipe-card',
   templateUrl: './swipe-card.component.html',
@@ -9,14 +9,32 @@ export class SwipeCardComponent implements OnInit {
   @ViewChild('card') card; 
   @Input() data: any;
   @Input() index: number;
+  xPosition: number;
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  onDragEnded(event): void {
+  public onDragEnded(event): void {
     event.source.element.nativeElement.style.transform = 'none';
     const source: any = event.source;
     source._dragRef.reset();
+  }
+  public handleDragStart(event): void {
+    this.xPosition = this._calcXPosition();
+    console.log(this.xPosition);
+  }
+  public handleDragDrop(event): void {
+    const x = this._calcXPosition();
+    // logic to determine left / right past threshold then trigger private swipe functions
+  }
+  private _calcXPosition(): number {
+    const rect = this.card.nativeElement.getBoundingClientRect();
+    return rect.x;
+  }
+  private _triggerLeftSwipe() {
+
+  }
+  private _triggerRightSwipe() {
+    
   }
 }
